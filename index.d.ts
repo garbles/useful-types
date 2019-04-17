b3a6dd3abac388d6c537a76fdde0413520ec3f39
Gabe Scholz <scholz.gabe@gmail.com>
2019-04-17 15:07:13 -0700

    Fix

--- index.d.ts
+++ index.d.ts
@@ -4,15 +4,23 @@ type KeyList<T> = T extends object
 
 // prettier-ignore
 type FlattenList<T> =
-  T extends [infer A, null] ? [A] :
-  T extends [infer A, [infer B, null] | null] ? [A, B] | [A] :
-  T extends [infer A, [infer B, [infer C, null] | null] | null] ? [A, B, C] | [A, B] | [A] :
-  T extends [infer A, [infer B, [infer C, [infer D, null] | null] | null] | null] ? [A, B, C, D] | [A, B, C] | [A, B] | [A] :
-  T extends [infer A, [infer B, [infer C, [infer D, [infer E, null] | null] | null] | null] | null] ? [A, B, C, D, E] | [A, B, C, D] | [A, B, C] | [A, B] | [A] :
-  T extends [infer A, [infer B, [infer C, [infer D, [infer E, [infer F, null] | null] | null] | null] | null] | null] ? [A, B, C, D, E, F] | [A, B, C, D, E] | [A, B, C, D] | [A, B, C] | [A, B] | [A] :
-  T extends [infer A, [infer B, [infer C, [infer D, [infer E, [infer F, [infer G, null] | null] | null] | null] | null] | null] | null] ? [A, B, C, D, E, F, G] | [A, B, C, D, E, F] | [A, B, C, D, E] | [A, B, C, D] | [A, B, C] | [A, B] | [A] :
-  T extends [infer A, [infer B, [infer C, [infer D, [infer E, [infer F, [infer G, [infer H, null] | null] | null] | null] | null] | null] | null] | null] ? [A, B, C, D, E, F, G, H] | [A, B, C, D, E, F, G] | [A, B, C, D, E, F] | [A, B, C, D, E] | [A, B, C, D] | [A, B, C] | [A, B] | [A] :
-  [];
+  T extends [infer A, infer B] ?
+    B extends null ? [A] :
+    B extends [infer C, infer D] ?
+      D extends null ? [A] | [A, C] :
+      D extends [infer E, infer F] ?
+        F extends null ? [A] | [A, C] | [A, C, E] :
+        F extends [infer G, infer H] ?
+          H extends null ? [A] | [A, C] | [A, C, E] | [A, C, E, G] :
+          H extends [infer I, infer J] ?
+            J extends null ? [A] | [A, C] | [A, C, E] | [A, C, E, G] | [A, C, E, G, I] :
+            J extends [infer K, infer L] ?
+              L extends null ? [A] | [A, C] | [A, C, E] | [A, C, E, G] | [A, C, E, G, I] | [A, C, E, G, I, K] :
+              L extends [infer M, infer N] ?
+                N extends null ? [A] | [A, C] | [A, C, E] | [A, C, E, G] | [A, C, E, G, I] | [A, C, E, G, I, K] | [A, C, E, G, I, K, M] :
+                N extends [infer O, infer P] ?
+                  P extends null ? [A] | [A, C] | [A, C, E] | [A, C, E, G] | [A, C, E, G, I] | [A, C, E, G, I, K] | [A, C, E, G, I, K, M] | [A, C, E, G, I, K, M, O] :
+  [] : [] : [] : [] : [] : [] : [] : [] : [];
 
 /**
  * Returns all valid key paths for a given type alias `T`.
